5869077b96977155f824a3becd83fd80815000c7
Raphael Saban <raphael.saban@gmail.com>
2020-12-17 13:16:12 -0600

    More friendly error

--- db/index.js
+++ db/index.js
@@ -1,5 +1,12 @@
 const { Sequelize } = require("sequelize");
 
+if (!process.env["DATABASE_URL"]) {
+  console.error(
+    'No database set. Please run `export DATABASE_URL="<your local db>"`'
+  );
+  process.exit(-1);
+}
+
 const sequelize = new Sequelize(process.env["DATABASE_URL"]);
 
 module.exports = sequelize;
